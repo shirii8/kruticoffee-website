93efb1fde487017f414a25e4ffd120142eca4ac0
garunAMT <21ME02022@iitbbs.ac.in>
2026-02-13 01:56:11 +0530

    fixed sidebar: added margins and removed elements

--- KrutiCoffee/src/context/StoreContext.jsx
+++ KrutiCoffee/src/context/StoreContext.jsx
@@ -1,42 +1,57 @@
-import {createContext, useEffect, useState} from 'react'
-import { food_list } from '../assets/frontend_assets/assets.js';
+import { createContext, useEffect, useState } from "react";
+import { food_list } from "../assets/frontend_assets/assets.js";
 
 export const StoreContext = createContext(null);
-const StoreContextProvider=(props)=>{
+const StoreContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState({});
+  const [token, setToken] = useState("");
 
-    const[cartItems, setCartItems]= useState({});
-
-    //functionality for add to cart
-    const addToCart=(itemId)=>{
-        if(!cartItems[itemId]){
-            setCartItems((prev)=>({...prev,[itemId]:1}))
-        }
-        else{
-            setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
-        }
+  //functionality for add to cart
+  const addToCart = (itemId) => {
+    if (!cartItems[itemId]) {
+      setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+    } else {
+      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
     }
+  };
 
-    const removeFromCart = (itemId) => {
-  setCartItems((prev) => ({
-    ...prev,
-    [itemId]: prev[itemId] - 1,
-  }));
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({
+      ...prev,
+      [itemId]: prev[itemId] - 1,
+    }));
+  };
+
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = food_list.find((product) => product._id === item);
+        if (itemInfo) {
+          totalAmount += itemInfo.price * cartItems[item];
+        }
+      }
+    }
+    return totalAmount;
+  };
+
+  useEffect(() => {}, [cartItems]);
+
+  const contextValue = {
+    food_list,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCart,
+    getTotalCartAmount,
+    token,
+    setToken,
+  };
+  return (
+    <StoreContext.Provider value={contextValue}>
+      {props.children}
+    </StoreContext.Provider>
+  );
 };
-
-
-    useEffect(()=>{
-    
-    }, [cartItems])
-
-
-    const contextValue = {
-        food_list, cartItems, setCartItems, addToCart, removeFromCart
-    }
-    return (
-        <StoreContext.Provider value={contextValue}>
-            {props.children}
-        </StoreContext.Provider>
-    )
-}
 
 export default StoreContextProvider;
--- KrutiCoffee/src/pages/Home.jsx
+++ KrutiCoffee/src/pages/Home.jsx
@@ -1,17 +1,19 @@
-import React, { useState } from 'react'
-import Navbar from '../components/Navbar.jsx';
-import FoodDisplay from '../components/FoodDisplay/FoodDisplay.jsx';
-import ExploreMenu from '../components/ExploreMenu/ExploreMenu.jsx';
+import React, { useState } from "react";
+import Navbar from "../components/Navbar.jsx";
+import FoodDisplay from "../components/FoodDisplay/FoodDisplay.jsx";
+import ExploreMenu from "../components/ExploreMenu/ExploreMenu.jsx";
 
 const Home = ({ setShowLogin }) => {
-  const [category, setCategory] = useState("All");
+  const [category, setCategory] = useState("All");
 
-  return (
-    <div className='w-full min-h-screen'>
-      <Navbar setShowLogin={setShowLogin} />
-      <ExploreMenu category={category} setCategory={setCategory} />
-      <FoodDisplay category={category} />
-    </div>
-  );
+  return (
+    <div className="w-full min-h-screen">
+      <Navbar setShowLogin={setShowLogin} />
+      <div className="ml-[120px]">
+        <ExploreMenu category={category} setCategory={setCategory} />
+        <FoodDisplay category={category} />
+      </div>
+    </div>
+  );
 };
 export default Home;
